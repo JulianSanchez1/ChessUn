6f7e3ab319acaa310870d99d6194c15d95637807
Julian Sanchez <juliandavidsanchezsanabria@gmail.com>
2020-07-02 23:57:40 -0500

    Añadiendo el movimiento del Rey

--- src/ChessUN/Rey.java
+++ src/ChessUN/Rey.java
@@ -22,9 +22,34 @@ public class Rey extends Pieza{
             color="/Multimedia/Piezas/ReyN.png";
         }
     }
-    public boolean canMove(int destination_x, int destination_y, ArrayList<Pieza> fichaE , ArrayList<Pieza> fichaA)
-    {       
-        return true;
+    public boolean canMove(int destino_x, int destino_y, ArrayList<Pieza> fichaE , ArrayList<Pieza> fichaA)
+    {     
+        if(destino_x==x+1 && ( (y - 1)== destino_y || (y + 1) == destino_y)){
+           return true;
+        }
+        if(destino_x==x-1 && ( (y - 1)== destino_y || (y + 1) == destino_y)){
+            return true;
+        }
+        if(destino_y==y+1 && ( (x - 1)== destino_x || (x + 1) == destino_x)){
+            return true;
+        }
+        if(destino_y==y-1 && ( (x - 1)== destino_x || (x + 1) == destino_x)){
+            return true;
+        }
+        
+        if(destino_y==y && x+1== destino_x ){
+            return true;
+        }
+        if(destino_y==y && x-1== destino_x ){
+            return true;
+        }
+        if(destino_y==y+1 && x== destino_x){
+            return true;
+        }
+        if(destino_y==y-1 && x== destino_x ){
+            return true;
+        }
+        return false;
     }
     public String getColor()
     {
