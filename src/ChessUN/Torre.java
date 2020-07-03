1473eb9ee4e39c1ec1dbbcf410a239cedcf29d26
Julian Sanchez <juliandavidsanchezsanabria@gmail.com>
2020-07-02 22:31:14 -0500

    Test

--- src/ChessUN/Torre.java
+++ src/ChessUN/Torre.java
@@ -27,42 +27,33 @@ public class Torre extends Pieza{
         boolean temp=true;
         if(destino_x==x &&  destino_y!=y){
             if(destino_y>y){
-                System.out.println(1);
                 for(int i = y+1 ; i < 8 ; i++){
-                    System.out.println("destino_x: "+destino_x+" i: "+i);
                     if(Ficha_Presente(destino_x,destino_y,fichaE)){
                         return true;
                     }
                     if(Ficha_Presente(destino_x,destino_y,fichaA)){
-                        System.out.println("Here");
                         return false; 
                     }
                     if( i==destino_y){
                         return true;
                     }
                     if(Ficha_Presente(destino_x,i,fichaE)||Ficha_Presente(destino_x,i,fichaA)){
-                        System.out.println("Here2");
                         return false;
                     }
                 }
             }
             else{
-                System.out.println(2);
-                
                 for(int i =y-1 ; i > -1 ; i--){
-                    System.out.println("destino_x: "+destino_x+" i: "+i);
                     if(Ficha_Presente(destino_x,destino_y,fichaE)){
                         return true;
                     }
                     if(Ficha_Presente(destino_x,destino_y,fichaA)){
-                        System.out.println("Here");
                         return false; 
                     }
                     if( i==destino_y){
                         return true;
                     }
                     if(Ficha_Presente(destino_x,i,fichaE)||Ficha_Presente(destino_x,i,fichaA)){
-                        System.out.println("Here2");
                         return false;
                     }
                 }               
@@ -71,21 +62,17 @@ public class Torre extends Pieza{
         if(destino_y==y &&  destino_x!=x){
             if(destino_x>x){
                 System.out.println(3);
-                for(int i = x ; i < 8 ; i++){
-                    
+                for(int i = x ; i < 8 ; i++){                 
                     if(Ficha_Presente(destino_x,destino_y,fichaE)){
                         return true;
                     }
                     if(Ficha_Presente(destino_x,destino_y,fichaA)){
-                        System.out.println("Here");
-                        
                         return false; 
                     }
                     if( i==destino_x){
                         return true;
                     }
                     if(Ficha_Presente(i,destino_y,fichaE)||Ficha_Presente(i,destino_y,fichaE)){
-                        System.out.println("Here2");
                         return false;
                     }
                 }
@@ -93,27 +80,23 @@ public class Torre extends Pieza{
             else{
                 System.out.println(4);
                 for(int i =x ; i > -1 ; i--){
-                    System.out.println("destino_x: "+destino_x+" i: "+i);
                     if( i==destino_x && Ficha_Presente(destino_x,destino_y,fichaE)){
                         return true;
                     }
-                    if( i==destino_x &&Ficha_Presente(destino_x,destino_y,fichaA)){
-                        System.out.println("Here");
-                        
+                    if( i==destino_x &&Ficha_Presente(destino_x,destino_y,fichaA)){                        
                         return false; 
                     }
                     if( i==destino_x){
                         return true;
                     }
                     if(Ficha_Presente(i,destino_y,fichaE)||Ficha_Presente(i,destino_y,fichaE)){
-                        System.out.println("Here2");
+                        System.out.println("BLABLA");
                         return false;
                     }
                 } 
                 return temp;
             }
         }
-        System.out.println("Fondo");
         return false;
     }
     public String getColor()
